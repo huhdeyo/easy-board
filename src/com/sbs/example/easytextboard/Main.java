a6405c56b9c1dc5bb2bc0028ebb305fd75c65bc1
huhdeyo <huhdeyo75@gmail.com>
2020-11-19 14:53:28 +0900

    게시물 등록시 정보표현

--- src/com/sbs/example/easytextboard/Main.java
+++ src/com/sbs/example/easytextboard/Main.java
@@ -7,6 +7,7 @@ public class Main {
 
 		String command;
 		Scanner scanner = new Scanner(System.in);
+		int id = 1;
 
 		while (true) {
 			System.out.printf("명령어 : ");
@@ -14,6 +15,18 @@ public class Main {
 
 			if (command.equals("article add")) {
 				System.out.println("==게시물 등록==");
+				
+				System.out.printf("제목 : ");
+				String title = scanner.nextLine();
+				System.out.printf("내용 : ");
+				String body = scanner.nextLine();
+				
+				System.out.println("==생성된 게시물 정보==");
+				System.out.println("번호 : " + id);
+				System.out.println("제목 : " + title);
+				System.out.println("내용 : " + body);
+				System.out.println("---------------------");
+				id++;
 			} else if (command.equals("article list")) {
 				System.out.println("==게시물 리스트");
 			} else if ( command.equals("exit")) {
