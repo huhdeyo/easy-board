03a55eae323af1880e939b04ef45a5eb018daa90
huhdeyo <huhdeyo75@gmail.com>
2020-11-19 14:23:59 +0900

    고객이 종료를 원할때까지 무한 반복

--- src/com/sbs/example/easytextboard/Main.java
+++ src/com/sbs/example/easytextboard/Main.java
@@ -5,20 +5,24 @@ import java.util.Scanner;
 public class Main {
 	public static void main(String[] args) {
 
-		String command ;
+		String command;
 		Scanner scanner = new Scanner(System.in);
 
-		System.out.printf("명령어 : ");
-		command = scanner.nextLine();
-		
-		if (command.equals("article add")) {
-			System.out.println("==게시물 등록==");
-		} else if ( command.equals("article list")) {
-			System.out.println("==게시물 리스트");
-		} else {
-			System.out.println("==존재하지 않는 명령어");
+		while (true) {
+			System.out.printf("명령어 : ");
+			command = scanner.nextLine();
+
+			if (command.equals("article add")) {
+				System.out.println("==게시물 등록==");
+			} else if (command.equals("article list")) {
+				System.out.println("==게시물 리스트");
+			} else if ( command.equals("exit")) {
+				break;
+			} else {
+				System.out.println("==존재하지 않는 명령어");
+			}
 		}
-		
+
 		scanner.close();
 	}
 }
